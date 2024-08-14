6474e115b4a6b1b23684ede12b8767805f50e03e
barturba <b@bartas.co>
2024-08-14 10:03:05 -0600

    Refactor users table schema to use 'text' data type for the 'name' column

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -3,8 +3,8 @@ CREATE TABLE users (
     id uuid PRIMARY KEY,
     created_at timestamp not null,
     updated_at timestamp not null,
-    name string not null
-)
+    name text not null
+);
 
 -- +goose Down
 DROP TABLE users;
