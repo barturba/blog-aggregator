372c003a46012a20818a0e654880d81b4dabf0b4
barturba <b@bartas.co>
2024-08-14 10:36:35 -0600

    chore: Update go.mod with new dependencies

--- main.go
+++ main.go
@@ -5,15 +5,23 @@ import (
 	"log"
 	"os"
 
+	"github.com/barturba/blog-aggregator/internal/database"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Couldn't load .env file")
 	}
-	fmt.Printf("PORT: %v\n", os.Getenv("PORT"))
+	port := os.Getenv("PORT")
+	databaseURL := os.Getenv("DATABASE_URL")
+	fmt.Printf("PORT: %v\n", port)
+	fmt.Printf("DATABASE_URL: %v\n", databaseURL)
 	fmt.Printf("the blog-aggregator has started\n")
 }
