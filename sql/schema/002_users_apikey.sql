dd74fa8a394752082fff62d520fa81bfd025ee1f
barturba <b@bartas.co>
2024-08-14 13:46:31 -0600

    feat: Added apikey column

---
+++ sql/schema/002_users_apikey.sql
@@ -0,0 +1,7 @@
+-- +goose Up
+ALTER TABLE users 
+ADD COLUMN apikey VARCHAR(64) unique not null 
+default encode(sha256(random()::text::bytea), 'hex');
+
+-- +goose Down
+ALTER TABLE users REMOVE COLUMN apikey;
