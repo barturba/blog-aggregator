a40aaad2d0e9877c28d0906aae508d754182a067
barturba <b@bartas.co>
2024-08-14 13:53:16 -0600

    feat: Add apikey column to User model and queries

--- internal/database/users.sql.go
+++ internal/database/users.sql.go
@@ -15,7 +15,7 @@ import (
 const createUser = `-- name: CreateUser :one
 INSERT into users (id, created_at, updated_at, name) 
 values ($1, $2, $3, $4)
-RETURNING id, created_at, updated_at, name
+RETURNING id, created_at, updated_at, name, apikey
 `
 
 type CreateUserParams struct {
@@ -38,6 +38,24 @@ func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (User, e
 		&i.CreatedAt,
 		&i.UpdatedAt,
 		&i.Name,
+		&i.Apikey,
+	)
+	return i, err
+}
+
+const getUser = `-- name: GetUser :one
+SELECT id, created_at, updated_at, name, apikey FROM users WHERE apikey = $1
+`
+
+func (q *Queries) GetUser(ctx context.Context, apikey string) (User, error) {
+	row := q.db.QueryRowContext(ctx, getUser, apikey)
+	var i User
+	err := row.Scan(
+		&i.ID,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+		&i.Name,
+		&i.Apikey,
 	)
 	return i, err
 }
--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -2,3 +2,6 @@
 INSERT into users (id, created_at, updated_at, name) 
 values ($1, $2, $3, $4)
 RETURNING *;
+
+-- name: GetUser :one
+SELECT * FROM users WHERE apikey = $1;
