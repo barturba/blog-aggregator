fee355bdc318a828ad383c6bbef818c3187abc4f
barturba <b@bartas.co>
2024-08-15 16:55:33 -0600

    Refactor handleFeedFollows to use UUID for FeedID and add FeedFollow struct

--- handle_feed_follows.go
+++ handle_feed_follows.go
@@ -3,13 +3,15 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/barturba/blog-aggregator/internal/database"
+	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handleFeedFollows(w http.ResponseWriter, r *http.Request, u database.User) {
 	type parameters struct {
-		FeedID string `json:"feed_id"`
+		FeedID uuid.UUID `json:"feed_id"`
 	}
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
@@ -20,7 +22,13 @@ func (cfg *apiConfig) handleFeedFollows(w http.ResponseWriter, r *http.Request, 
 	}
 
 	feedFollow, err := cfg.DB.CreateFeedFollow(r.Context(),
-		database.CreateFeedFollowsParams{})
+		database.CreateFeedFollowParams{
+			ID:        uuid.New(),
+			CreatedAt: time.Time{},
+			UpdatedAt: time.Time{},
+			FeedID:    params.FeedID,
+			UserID:    u.ID,
+		})
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't create feed")
 		return
--- models.go
+++ models.go
@@ -52,3 +52,21 @@ func databaseFeedsToFeeds(feeds []database.Feed) []Feed {
 	}
 	return items
 }
+
+type FeedFollow struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	FeedID    uuid.UUID `json:"feed_id"`
+	UserID    uuid.UUID `json:"user_id"`
+}
+
+func databaseFeedFollowToFeedFollow(feedFollow database.FeedFollow) FeedFollow {
+	return FeedFollow{
+		ID:        feedFollow.ID,
+		CreatedAt: feedFollow.CreatedAt,
+		UpdatedAt: feedFollow.UpdatedAt,
+		FeedID:    feedFollow.FeedID,
+		UserID:    feedFollow.UserID,
+	}
+}
