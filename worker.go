3992e8b5565c8b38865eaa48dd3ba85f7f516eb0
barturba <b@bartas.co>
2024-08-17 14:23:36 -0600

    refactor: Improve logging in runWorker function

--- worker.go
+++ worker.go
@@ -11,9 +11,7 @@ import (
 )
 
 func (c *apiConfig) runWorker(wg *sync.WaitGroup, maxFeeds int, workerDelay time.Duration) {
-	fmt.Printf("starting main worker\n")
-	fmt.Printf("maxFeeds %v\n", maxFeeds)
-	fmt.Printf("workerDelay %v\n", workerDelay)
+	fmt.Printf("starting main worker. maxFeeds %v, workerDelay %v\n", maxFeeds, workerDelay)
 
 	ctx := context.Background()
 	var feedsToFetch []database.Feed
@@ -21,21 +19,15 @@ func (c *apiConfig) runWorker(wg *sync.WaitGroup, maxFeeds int, workerDelay time
 
 	for {
 
-		fmt.Printf("fetching data from database\n")
 		time.Sleep(1 * time.Second)
-		// TODO: GetNextFeedsToFetch should accept input for n
 		feedsToFetch, err = c.DB.GetNextFeedsToFetch(ctx, int32(maxFeeds))
 		if err != nil {
 			log.Printf("error when getting feeds from database: %s\n", err)
 			return
 		}
-		fmt.Printf("done fetching data from database\n")
-		fmt.Printf("len(feedsToFetch): %v\n", len(feedsToFetch))
 		for _, feed := range feedsToFetch {
 			fmt.Printf("will fetch data for feed %s, last fetched: %v\n", feed.Name, feed.LastFetchedAt)
 		}
-
-		// for each feed fetch and process it using a goroutine
 
 		for _, feed := range feedsToFetch {
 			wg.Add(1)
@@ -49,7 +41,6 @@ func (c *apiConfig) runWorker(wg *sync.WaitGroup, maxFeeds int, workerDelay time
 				fmt.Printf("stopped data fetching worker for feed %s\n", feed.ID.String())
 
 				fmt.Printf("starting data processing worker for feed %s\n", feed.ID.String())
-				time.Sleep(time.Second * 10)
 				c.Client.ProcessRSS(data)
 				fmt.Printf("stopped data processing worker for feed %s\n", feed.ID.String())
 			}()
