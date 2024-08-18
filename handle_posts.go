90e1c7186c8a318d76c82c346f8638f5147615f2
barturba <b@bartas.co>
2024-08-18 15:46:01 -0600

    refactor: Add limit parameter to getPosts endpoint handler

--- handle_posts.go
+++ handle_posts.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/barturba/blog-aggregator/internal/database"
 )
 
 func (cfg *apiConfig) getPosts(w http.ResponseWriter, r *http.Request, u database.User) {
+	fmt.Println("GET params were:", r.URL.Query())
+	limit := 10
+	limitParam := r.URL.Query().Get("limit")
+	if limitParam != "" {
+		fmt.Println("limit:", limitParam)
+		var err error
+		limit, err = strconv.Atoi(limitParam)
+		if err != nil {
+			log.Printf("error parsing limit param")
+			limit = 10
+		}
+	}
 
 	posts, err := cfg.DB.GetPostsByUser(r.Context(),
 		database.GetPostsByUserParams{
 			UserID: u.ID,
-			Limit:  10})
+			Limit:  int32(limit)})
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't get posts by user")
 		return
