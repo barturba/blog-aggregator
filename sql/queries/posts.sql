e66f1526b73fbb9b27504ee4842834be412e75c1
barturba <b@bartas.co>
2024-08-18 12:56:48 -0600

    refactor: Update GetPostsByUser query to order posts by published date

--- sql/queries/posts.sql
+++ sql/queries/posts.sql
@@ -4,8 +4,9 @@ INSERT into posts(id, created_at, updated_at, title, url, description, published
 values ($1, $2, $3, $4, $5, $6, $7, $8)
 RETURNING *;
 
--- -- name: GetFeedFollows :many
--- SELECT * FROM feed_follows where user_id = $1;
+-- -- name: GetPostsByUser:many
+SELECT * FROM posts where user_id = $1
+ORDER BY published_at DESC;
 
 -- -- name: DeleteFeedFollows :exec
 -- DELETE FROM feed_follows
